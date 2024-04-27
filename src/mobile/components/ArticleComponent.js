20c05d2c16a4a4a07f43f3d2d806af7a6cd0c5a4
Pham Minh Tam <100601043+21020391@users.noreply.github.com>
2024-04-27 13:34:20 +0700

    divide folder articles

--- src/mobile/components/ArticleComponent.js
+++ src/mobile/components/ArticleComponent.js
@@ -2,9 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { View, StyleSheet, Text, Pressable } from "react-native";
 import { Padding, Color, ColorDark, FontFamily, Border, FontSize } from "../GlobalStyles";
-import ButtonPrimary from "./ButtonPrimary";
+import ButtonPrimary from "./button/ButtonPrimary";
 
-const ArticleComponent =({
+const ArticleComponent = ({
     article,
     date,
     text,
@@ -12,12 +12,12 @@ const ArticleComponent =({
     const isDarkMode = useSelector(state => state.theme.isDarkMode);
 
     return (
-        <View style={{padding: 10}}>
-            <Text style={[styles.username, styles.usernameTypo, isDarkMode ? {color: ColorDark.surfaceOnSurfaceVarient} : null]}>{article}</Text>
-            <Text style={[styles.ddmmyyyy, styles.usernameTypo, isDarkMode ? {color: ColorDark.surfaceOnSurfaceVarient} : null]}>{date}</Text>
-            <Text style={[styles.text, isDarkMode ? {color: ColorDark.surfaceOnSurface} : null]}>{text}</Text>
-            <Pressable style={{marginTop: 5}}>
-                <Text style={[styles.readMore, isDarkMode ? {color: ColorDark.primaryPrimary} : null]}>Read more</Text>
+        <View style={{ padding: 10 }}>
+            <Text style={[styles.username, styles.usernameTypo, isDarkMode ? { color: ColorDark.surfaceOnSurfaceVarient } : null]}>{article}</Text>
+            <Text style={[styles.ddmmyyyy, styles.usernameTypo, isDarkMode ? { color: ColorDark.surfaceOnSurfaceVarient } : null]}>{date}</Text>
+            <Text style={[styles.text, isDarkMode ? { color: ColorDark.surfaceOnSurface } : null]}>{text}</Text>
+            <Pressable style={{ marginTop: 5 }}>
+                <Text style={[styles.readMore, isDarkMode ? { color: ColorDark.primaryPrimary } : null]}>Read more</Text>
             </Pressable>
             <View
                 style={{
@@ -37,12 +37,12 @@ const styles = StyleSheet.create({
         textAlign: "left",
     },
     username: {
-    fontSize: FontSize.labelLargeBold_size,
-    color: Color.surfaceOnSurfaceVarient,
+        fontSize: FontSize.labelLargeBold_size,
+        color: Color.surfaceOnSurfaceVarient,
     },
     ddmmyyyy: {
-    fontSize: FontSize.labelSmallRegular_size,
-    color: Color.surfaceOnSurfaceVarient,
+        fontSize: FontSize.labelSmallRegular_size,
+        color: Color.surfaceOnSurfaceVarient,
     },
     text: {
         fontFamily: FontFamily.typographyLabelLarge,
